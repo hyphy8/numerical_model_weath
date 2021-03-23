2eae44eef29559acee9dc3e1d2beb50e206a3136
hyphy8 <62756046+hyphy8@users.noreply.github.com>
2021-03-22 20:38:05 -0400

    Add files via upload
    Jan00 wrfout data for post-processing. WRF run on Cheyenne.

---
+++ wrfJAN00_post.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from osgeo import gdal
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy
+import os
+
+
+# In[ ]:
+
+
+
+
